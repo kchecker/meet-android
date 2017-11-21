6e6463aa65e4291f4e07879e9d229a6afb54c027
sachininp <sachinipadmasiri425@gmail.com>
2017-11-21 10:16:05 +0530

    Remove Light Image

--- app/src/main/java/com/example/jitsiandroid/MainActivity.java
+++ app/src/main/java/com/example/jitsiandroid/MainActivity.java
@@ -24,7 +24,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends JitsiMeetActivity implements SpeechRecognizerManager.OnResultListener{
     private final String TAG = getClass().getSimpleName();
-    private ImageView lightBulbImageView;
+//    private ImageView lightBulbImageView;
     private SpeechRecognizerManager mSpeechRecognizerManager;
     @Override
     protected JitsiMeetView initializeView() {
@@ -103,34 +103,34 @@ public class MainActivity extends JitsiMeetActivity implements SpeechRecognizerM
         super.onCreate(savedInstanceState);
 
         //speech recognize
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
 
-        lightBulbImageView = (ImageView) findViewById(R.id.lightBulbImageView);
+//        lightBulbImageView = (ImageView) findViewById(R.id.lightBulbImageView);
         mSpeechRecognizerManager =new SpeechRecognizerManager(this);
         mSpeechRecognizerManager.setOnResultListner(this);
     }
-    private void hideLightBulb(){
-        lightBulbImageView.setVisibility(View.INVISIBLE);
-    }
-
-    private void showLightBulb(){
-        lightBulbImageView.setVisibility(View.VISIBLE);
-    }
-
-    private void turnYellow(){
-        lightBulbImageView.setImageResource(R.drawable.yellow);
-        lightBulbImageView.setVisibility(View.VISIBLE);
-    }
-
-    private void turnBlue(){
-        lightBulbImageView.setImageResource(R.drawable.blue);
-        lightBulbImageView.setVisibility(View.VISIBLE);
-    }
-
-    private void turnPink(){
-        lightBulbImageView.setImageResource(R.drawable.pink);
-        lightBulbImageView.setVisibility(View.VISIBLE);
-    }
+//    private void hideLightBulb(){
+//        lightBulbImageView.setVisibility(View.INVISIBLE);
+//    }
+//
+//    private void showLightBulb(){
+//        lightBulbImageView.setVisibility(View.VISIBLE);
+//    }
+//
+//    private void turnYellow(){
+//        lightBulbImageView.setImageResource(R.drawable.yellow);
+//        lightBulbImageView.setVisibility(View.VISIBLE);
+//    }
+//
+//    private void turnBlue(){
+//        lightBulbImageView.setImageResource(R.drawable.blue);
+//        lightBulbImageView.setVisibility(View.VISIBLE);
+//    }
+//
+//    private void turnPink(){
+//        lightBulbImageView.setImageResource(R.drawable.pink);
+//        lightBulbImageView.setVisibility(View.VISIBLE);
+//    }
 
     @Override
     public void OnResult(ArrayList<String> commands) {
@@ -139,31 +139,31 @@ public class MainActivity extends JitsiMeetActivity implements SpeechRecognizerM
         {
             if (command.equals("hide")){
                 Toast.makeText(this,"You said:"+command, Toast.LENGTH_SHORT).show();
-                hideLightBulb();
+//                hideLightBulb();
                 return;
             }
 
             if (command.equals("show")){
                 Toast.makeText(this,"You said:"+command, Toast.LENGTH_SHORT).show();
-                showLightBulb();
+//                showLightBulb();
                 return;
             }
 
             if (command.equals("turn yellow")){
                 Toast.makeText(this,"You said:"+command, Toast.LENGTH_SHORT).show();
-                turnYellow();
+//                turnYellow();
                 return;
             }
 
             if (command.equals("turn blue")){
                 Toast.makeText(this,"You said:"+command, Toast.LENGTH_SHORT).show();
-                turnBlue();
+//                turnBlue();
                 return;
             }
 
             if (command.equals("turn pink")){
                 Toast.makeText(this,"You said:"+command, Toast.LENGTH_SHORT).show();
-                turnPink();
+//                turnPink();
                 return;
             }
         }
